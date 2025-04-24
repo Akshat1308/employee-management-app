8749b1c429229ece2116fbef7d94e904412ad95e
Akshat Surana <akshatsurana001@gmail.com>
2025-04-24 16:51:49 +0530

    Switch from SQLite to PostgreSQL

--- app.py
+++ app.py
@@ -146,6 +146,6 @@ def edit_employee(emp_id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Create all database tables if they don't exist
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
